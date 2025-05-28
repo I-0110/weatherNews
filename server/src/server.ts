06846e6afa33306468ad153f53f080f6b69f2f3e
IvelisBecker <ivelisbecker@gmail.com>
2025-05-28 18:28:08 -0500

    All commands are working!

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 // Import the routes
 import routes from './routes/index.js';
 
@@ -15,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(express.static(`../client/dist`));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
